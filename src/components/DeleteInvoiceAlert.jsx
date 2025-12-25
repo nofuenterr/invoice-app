75f6563c7d831ed4b0498576ee38ee1037d22bd6
RR <rrn091301@gmail.com>
2025-12-25 20:44:40 +0800

    feat: add styles and responsiveness to delete invoice alert dialog

--- src/components/DeleteInvoiceAlert.jsx
+++ src/components/DeleteInvoiceAlert.jsx
@@ -10,20 +10,26 @@ export default function DeleteInvoiceAlert({
 		<AlertDialog.Root>
 			<AlertDialog.Trigger asChild>{children}</AlertDialog.Trigger>
 			<AlertDialog.Portal>
-				<AlertDialog.Overlay />
-				<AlertDialog.Content>
-					<AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-					<AlertDialog.Description>
-						Are you sure you want to delete invoice #{id}? This action cannot be
-						undone.
-					</AlertDialog.Description>
-					<div className="flex justify-end gap-6">
+				<AlertDialog.Overlay className="bg-overlay fixed inset-0" />
+				<AlertDialog.Content className="dark:bg-03 fixed top-1/2 left-1/2 z-20 grid w-4/5 max-w-120 -translate-x-1/2 -translate-y-1/2 gap-5.5 rounded-lg bg-white p-8 md:gap-3.5 md:p-12">
+					<div className="grid gap-2 md:gap-3">
+						<AlertDialog.Title className="text-08 text-2xl font-bold dark:text-white">
+							Confirm Deletion
+						</AlertDialog.Title>
+						<AlertDialog.Description className="text-06 dark:text-05">
+							Are you sure you want to delete invoice #{id}? This action cannot
+							be undone.
+						</AlertDialog.Description>
+					</div>
+					<div className="flex justify-end gap-2 text-[15px] font-bold">
 						<AlertDialog.Cancel asChild>
-							<button>Cancel</button>
+							<button className="bg-17 hover:bg-05 hover:dark:text-07 text-07 dark:text-05 dark:bg-04 cursor-pointer rounded-3xl px-6 py-4 hover:dark:bg-white">
+								Cancel
+							</button>
 						</AlertDialog.Cancel>
 						<AlertDialog.Action asChild>
 							<button
-								className="bg-09"
+								className="bg-09 hover:bg-10 cursor-pointer rounded-3xl px-6 py-4 text-white"
 								onClick={() => {
 									deleteInvoice(id);
 									navigate(-1);
