df311dba0f3235658dc9c63c934df7f85f3cc670
RR <rrn091301@gmail.com>
2025-12-26 18:43:46 +0800

    fix: import assets instead of using absolute paths for images to render in production

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -2,6 +2,8 @@ import { useThemeStore } from '../stores/themeStore';
 import { Switch, Avatar, Separator } from 'radix-ui';
 import SunIcon from './ui/SunIcon';
 import MoonIcon from './ui/MoonIcon';
+import logo from '../assets/ui/logo.svg';
+import avatar from '../assets/ui/image-avatar.jpg';
 
 export default function HeaderBar() {
 	const { dark, switchTheme } = useThemeStore();
@@ -9,7 +11,7 @@ export default function HeaderBar() {
 	return (
 		<header className="bg-13 flex h-18 items-center gap-6 md:h-20 md:gap-8 lg:h-dvh lg:w-25 lg:flex-col lg:gap-6 lg:rounded-tr-[20px] lg:rounded-br-[20px]">
 			<div className="bg-01 relative mr-auto grid aspect-square size-18 place-content-center overflow-hidden rounded-tr-[20px] rounded-br-[20px] md:size-20 lg:mb-auto lg:size-25">
-				<img src="/src/assets/ui/logo.svg" alt="Brand logo" className="z-10" />
+				<img src={logo} alt="Brand logo" className="z-10" />
 				<div className="bg-02 absolute top-1/2 grid aspect-square size-full place-content-center rounded-tl-[20px] rounded-bl-[20px]"></div>
 			</div>
 
@@ -35,7 +37,7 @@ export default function HeaderBar() {
 				<Avatar.Image
 					className="size-8 rounded-full lg:size-10"
 					src="/src/assets/ui/image-avatar.jpg"
-					alt="Image avatar"
+					alt={avatar}
 				/>
 				<Avatar.Fallback
 					className="bg-11 text-02 flex size-full place-content-center text-base leading-none font-medium"
